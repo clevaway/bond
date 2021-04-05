5a20dd8be918bccc32895dc5cc7b5163e7960492
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-05 13:38:36 +0100

    folder for all auth

---
+++ ndole/bond_app/lib/auth/wrapper.dart
@@ -0,0 +1,17 @@
+import 'package:bond_app/auth/google_sign_in.dart';
+import 'package:bond_app/models/user_models.dart';
+import 'package:bond_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserModels>(context);
+    if (user == null) {
+      return GoogleSignInScreen();
+    } else {
+      return HomeScreen();
+    }
+  }
+}
