5310b72cc7c396a762155949f2c2d43b02c66213
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-05 08:19:11 +0100

    where all the core designs will be kept

---
+++ ndole/bond_app/lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bond',
+    );
+  }
+}
