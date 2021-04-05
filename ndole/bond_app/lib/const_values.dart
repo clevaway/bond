fcc36251bf24ce32b32825bc9d23e375c8df6e0f
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-05 08:21:03 +0100

    added some values

---
+++ ndole/bond_app/lib/const_values.dart
@@ -0,0 +1,8 @@
+import 'package:bond_app/utils/hex_to_color.dart';
+import 'package:flutter/material.dart';
+
+const Color containerColor = Color.fromRGBO(42, 44, 49, 1);
+const Color boxColor = Color.fromRGBO(235, 237, 242, 1);
+
+Color textLight = hexToColor("#ffffff");
+Color textDark = hexToColor("#0e0e0f");
