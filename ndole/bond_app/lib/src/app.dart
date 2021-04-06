4710d240a0f1c901182a1e563afc3f569f97607b
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-06 13:38:48 +0100

    called the provider scope

--- ndole/bond_app/lib/src/app.dart
+++ ndole/bond_app/lib/src/app.dart
@@ -1,6 +1,8 @@
 import 'package:bond_app/auth/wrapper.dart';
 import 'package:bond_app/custom_theme.dart';
+import 'package:bond_app/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatefulWidget {
   @override
@@ -10,10 +12,11 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
+    final checkTheme = Provider.of<ThemeProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bond',
-      theme: buildLightTheme(),
+      theme: checkTheme.mTheme == false ? buildLightTheme() : buildDarkTheme(),
       home: Wrapper(),
     );
   }
