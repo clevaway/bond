fb9675d6eef343f869e805af0f5fb4f3544df210
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2021-04-16 14:50:18 +0100

    Splited the node-api from the websocket server scripts to make it look clean

--- eru/src/main.js
+++ eru/src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
+  Vuesax,
   render: h => h(App)
 }).$mount('#app')
