2fd898f697da126b450def9c70f7a4834dfe11b6
TatchumNono <tatchumnono@gmail.com>
2021-04-24 14:29:15 +0200

    Created the one of the profile avatar components of the figma design

---
+++ eru/src/components/userComponents/ProfileAvatar.vue
@@ -0,0 +1,28 @@
+<template>
+    <span>
+            <v-avatar
+                color="neutralgray"
+                size="200"
+                class="profileBorder"
+            >
+                <img
+                    :src= "img"
+                    alt="John"
+                >
+            </v-avatar>
+    </span>
+</template>
+
+<script>
+export default {
+    props:{
+        img: String
+    },
+}
+</script>
+
+<style>
+.profileBorder{
+ border: 8px solid;
+}
+</style>
