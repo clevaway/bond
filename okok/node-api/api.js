0f5ef02de25ee9e0de0166f7e82bebf26dddefc6
joseph <josephjunejoe@yahoo.com>
2021-05-03 23:03:27 +0800

    editUser: escape string,catching object,empty

--- okok/node-api/api.js
+++ okok/node-api/api.js
@@ -124,39 +124,53 @@ const sendInvite = async (request, response) => {
 
 // endpoint to edit user info
 const editUser = async (request, response) => {
-  //initialize return type of error
+
+  //initialize return object of error
   var returnErr = {
     message: "Invalid Field",
     status: 1
   }
 
-  //checking all fields for undefined/garbage values 
-  if(request.body.name != undefined || request.body.username != undefined ||request.body.photo != undefined || request.body.uid != undefined){
-    response.status(500).json(returnErr)
-    return
+  //initialize person to return to response
+  var personReturn = {
+    name: request.body.name,
+    username: request.body.username,
+    photo: request.body.photo
   }
+
+  //checking all fields for null/garbage values 
+  if(typeof request.body.name === 'object' ||typeof request.body.username === 'object' ||typeof request.body.uid === 'object'){
+    return response.status(500).json(returnErr)
+  }
+
+  //not allowing username and name to have special characters
+  personReturn.name = request.body.name.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\//\s]/gi, '');
+  personReturn.username = request.body.username.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\//\s]/gi, '');
 
   //checking all fields for empty values
-  if(request.body.name != "" || request.body.username != "" ||request.body.photo != "" || request.body.uid != ""){
-    returnErr.status = 2
-    returnErr.message = "Cannot update profile with empty fields"
-    response.status(500).json(returnErr)
-    return
+  if(personReturn.name == "" || personReturn.username == "" ||request.body.photo == "" || request.body.uid == ""){
+    returnErr.status = 2;
+    returnErr.message = "Cannot update profile with empty fields";
+    return response.status(500).json(returnErr)
   }
-    
+  
 
   //updating the person info there is no error
-  con.query("UPDATE person SET name='"+request.body.name+"',username='"+request.body.username+"',photo='"+request.body.photo+"' WHERE uid='"+request.body.uid+"'", function (err, result) {
-    // checking if uid exists and profile was updated successfully
-   if(result == undefined || result.length == 0){
-      returnErr.status = 3
-      returnErr.message = "uid doesnt exist"
-      response.status(500).json(returnErr)
-      return 
-   }
+  con.query("UPDATE person SET name='"+personReturn.name+"',username='"+personReturn.username+"',photo='"+request.body.photo+"' WHERE uid='"+request.body.uid+"'", function (err, result) {
 
-    //if success
-    response.status(200).json(person)
+    //checking if uid exists and profile was updated successfully
+    if(result.affectedRows == 0 ){
+      returnErr.status = 3;
+      returnErr.message = "uid doesnt exist";
+      return response.status(500).json(returnErr)
+    }
+
+    
+
+    //if successfull
+    response.status(200).json(personReturn)
+   
+    
    })
 
 
