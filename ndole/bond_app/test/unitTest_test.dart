b204a1de816ae711815b1af772aed99f5242b5fc
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-05-10 11:55:08 +0100

    trying to write test for app

---
+++ ndole/bond_app/test/unitTest_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('One should be One', () {
+    // Arrange
+    int expectedNumber = 2;
+
+    // Act
+
+    // Assert
+    expect(expectedNumber, 1);
+  });
+}
