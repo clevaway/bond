6112f451152345c0ac8c5daf8ffb78597d6daa2c
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2021-05-27 14:36:00 +0100

    Implemented the vibration functionality on the web, both users bonded get a vibration on both devices when they click on they hard in real time

--- eru/src/plugins/socketio.js
+++ eru/src/plugins/socketio.js
@@ -1,5 +1,6 @@
 // plugin for websocket connection
 import io from 'socket.io-client';
 // add http://localhost:3000 if you want to run the websocket server locally
+// const socket = io('http://localhost:3000', { transports: ['websocket'] });
 const socket = io('bond-websockets.glitch.me', { transports: ['websocket'] });
 export default socket;
